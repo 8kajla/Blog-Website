8e466692ec7b9eff6071f957349e57bab0af2c47
unknown <kajlad99@gmail.com>
2026-05-07 11:41:45 +0530

    css fix-2

--- blogwebsite/settings.py
+++ blogwebsite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,3 +128,4 @@ STATICFILES_DIRS=[
     BASE_DIR / 'blogwebsite'/'static'
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
